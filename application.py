80d844a7a4e07764b2ee4806f732a75b50231b94
jhapranav512 <jhapk1205@gmail.com>
2023-01-29 14:48:44 -0600

    Initial commit

---
+++ application.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+application = Flask(__name__)
+response = list()
+
+
+@application.route("/")
+def say_hello():
+    return jsonify("Hello from Flask Application")
+
+
+@application.route("/name")
+def say_hello_from_request():
+    name = request.json.get("name")
+    return jsonify("hello " + name)
+
+
+@application.route("/adddata", methods=["POST"])
+def add_data():
+    data = dict()
+    name = request.json.get("name")
+    age = request.json.get("age")
+    data["name"] = name
+    data["age"] = age
+    response.append(data)
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    application.run()
